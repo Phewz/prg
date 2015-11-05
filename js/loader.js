8212ae1301d499fa4c3e59aa02d35d5e418bb8a5
Phewz <deax05@gmail.com>
2015-11-05 15:56:18 +0100

    logo + loader integration
    Test der logos folgt.

--- js/loader.js
+++ js/loader.js
@@ -1,1 +1,63 @@
-// soon. loading script incl. animation integration
+// To Do: In HTML integrieren, Test ob * funktioniert
+
+var qdots = ["", ".", "..", "..."];
+
+// ** bug -v
+function show_image(src) {
+    var logo = document.createElement("logo");
+    logo.src = "images/Logo.png";
+
+    document.body.appendChild(img);
+}
+// ** but ^-
+window.addEventListener('load', function() {
+  var canvas = document.createElement("canvas"),
+      context = canvas.getContext('2d');
+
+  function resize() {
+    canvas.width  = window.innerWidth;
+    canvas.height = window.innerHeight;
+  } resize();
+
+  window.addEventListener('resize', resize);
+
+  document.body.appendChild(canvas);
+
+  function rad(deg) {
+    return (deg * 0.0174532925);
+  }
+
+  function deg(rad) {
+    return (rad * 57.2957795);
+  }
+
+  +(function test(time) {
+    var x = .5 + canvas.width / 2,
+        y = .5 + canvas.height / 2,
+        d = (((time || Date.now()) / 3) % 720),
+        text, m;
+
+    context.lineCap = 'round';
+    context.lineWidth = 3;
+    context.font = '500 14px Futura';
+    context.textBaseline = 'middle';
+
+    context.fillStyle = 'rgba(38, 38, 38, .9)';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.beginPath();
+      if(d < 360) {
+        context.arc(x, y, 50, 0, rad(d));
+      } else {
+        context.arc(x, y, 50, rad(d - 360), 0);
+      }
+      context.strokeStyle = context.fillStyle = '#cecece';
+      context.stroke();
+    context.closePath();
+    // bug **
+    show_image;
+    text = 'Loading Drum Machine', m = context.measureText(text);
+    context.fillText(text + qdots[Math.floor(d / 180)], (x - (m.width / 2)), y);
+
+    requestAnimFrame(test); // polyfill in codepen-utilities.js
+  }());
+})
