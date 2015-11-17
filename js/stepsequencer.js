0db234b4c6adb5be74ad8e3586a5aee90835ab75
Phewz <deax05@gmail.com>
2015-11-17 14:27:05 +0100

    update stepsequencer
    ziemlich buggy, aber aktueller stand

--- js/stepsequencer.js
+++ js/stepsequencer.js
@@ -5,11 +5,7 @@ Es gibt eine Sequenz von 4 Takten mit je 4 Schlägen. Die Sequenz wird mit dem v
 Nach einnem Durchlauf wird diese wieder von vorne abgespielt.
 Es handelt sich hier also um eine sich wiederholende Sequenz aus insgesamt 16 Schlägen in einem 4/4 Takt.
 
-
-if 1 = play sound
-else = skip sound
-
-tempo gibt geschwindigkeit in BPM an. Auslagerung in clock.js
+tempo gibt geschwindigkeit in BPM an. Auslagerung in clock.js?
 
  */
 
@@ -20,7 +16,7 @@ tempo gibt geschwindigkeit in BPM an. Auslagerung in clock.js
 		 var canvas = document.getElementById(canvasID);
 		 var screen = canvas.getContext("2d");
 		 var audioContext = new AudioContext();
-		 // red, grey
+		 // Rot, Grau
 		 var color = [{active : ["FF0000"], off : ["#D3D3D3"]}];
 		 
 		 // Eigenschaften
@@ -28,6 +24,35 @@ tempo gibt geschwindigkeit in BPM an. Auslagerung in clock.js
 		 this.samples = {};
 		 
 		 
-	 }
-	 
+		 this.mouseInput = new SequencerMouseInput(canvas, this);
+		 this.ticker = new Ticker();
+		 
+		 // Step Eigenschaften
+		 var self = this;
+		 var startSequencer = function() {
+			 self.step();	 
+		 };
+		 
+		 // Sequencer zeichnen, 60fps
+		 this.runUpdate = function() {
+			 self.draw(screen);
+		 }
+		 this.ticker.every(17, this.runUpdate);
+		 
+		 // Takt angeben
+		 
+		 var lastID;
+		 this.setTempo = function(tempo){
+			 // Tempo anpassen
+			 tempo = (tempo < 0) ? 0 : ((tempo > 300) ? 300 : tempo);
+			 this.ticker.delete(lastID);
+			 lastID = this.ticker.every(this.tempoToTimestep(tempo), startSequencer);
+		 }
+		 
+		this.setupUI(formId);
+		this.displayView = "all";
+		 
+		this.tempoSelect = document.getElementById(tempoId);
+		this.setupEventHandlers();
+		};
  }
