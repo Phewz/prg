cbd754a6af63156f95a049d76d712d8277bd3e36
Phewz <deax05@gmail.com>
2015-11-17 14:05:50 +0100

    Update stepsequencer.js

--- js/stepsequencer.js
+++ js/stepsequencer.js
@@ -13,21 +13,6 @@ tempo gibt geschwindigkeit in BPM an. Auslagerung in clock.js
 
  */
 
-/*
-
-geplant:
-Es gibt eine Sequenz von 4 Takten mit je 4 Schlägen. Die Sequenz wird mit dem vorgegebenen Tempo abgespielt.
-Nach einnem Durchlauf wird diese wieder von vorne abgespielt.
-Es handelt sich hier also um eine sich wiederholende Sequenz aus insgesamt 16 Schlägen in einem 4/4 Takt.
-
-
-if 1 = play sound
-else = skip sound
-
-tempo gibt geschwindigkeit in BPM an. Auslagerung in clock.js
-
- */
-
  (function(){
 	 
 	 var StepSequencer = function(canvasID, tempoID, formID) {
